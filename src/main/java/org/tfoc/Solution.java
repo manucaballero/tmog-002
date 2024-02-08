6d3525fb801a6a0f740ca9f45c88a19150ad0aa8
Manuel Caballero Rabago <mcabalra@mercadona.es>
2024-02-08 15:52:00 +0100

    Solved galadrial 002

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -7,7 +7,59 @@ public class Solution {
 
     public boolean isValid(String s) {
 
-        return false;
+        char[] characters = s.toCharArray();
+
+        if(characters.length % 2 != 0) return false;
+
+        boolean isValid = true;
+        int counter = 0;
+
+        while (isValid && counter < characters.length){
+
+            char firstChar = characters[counter];
+            char secondChar = characters[counter + 1];
+
+            if((isOpenParentheses(firstChar) && !isClosingParentheses(secondChar))
+            || (isOpenBracket(firstChar) && !isClosingBracket(secondChar))
+            || (isOpenCurlyBrace(firstChar) && !isClosingCurlyBrace(secondChar))){
+
+                isValid = false;
+            }
+
+            counter+= 2;
+        }
+
+        return isValid;
+    }
+
+    private boolean isOpenParentheses(char element){
+
+        return element == 40;
+    }
+
+    private boolean isClosingParentheses(char element){
+
+        return element == 41;
+    }
+
+    private boolean isOpenBracket(char element){
+
+        return element == 91;
+    }
+
+    private boolean isClosingBracket(char element){
+
+        return element == 93;
+    }
+
+    private boolean isOpenCurlyBrace(char element){
+
+        return element == 123;
+    }
+
+    private boolean isClosingCurlyBrace(char element){
+
+        return element == 125;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -3,13 +3,50 @@ package org.tfoc;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @Slf4j
 class SolutionTest {
 
     @Test
-    void testSolution() {
+    void testSolutionExample1() {
+        Solution s = new Solution();
+        boolean result = s.isValid("()");
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        assertTrue(result);
+    }
+
+    @Test
+    void testSolutionExample2() {
+        Solution s = new Solution();
+        boolean result = s.isValid("()[]{}");
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testSolutionExample3() {
+        Solution s = new Solution();
+        boolean result = s.isValid("(]");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testSolutionNonEvenSizeString() {
+        Solution s = new Solution();
+        boolean result = s.isValid("(])");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testSolutionFirstPairCorrectSecondIncorrect() {
+        Solution s = new Solution();
+        boolean result = s.isValid("(){]");
+
+        assertFalse(result);
     }
 
 }
